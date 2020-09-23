8943b3d013406bd87c7847e523d413fa656574bb
Frai Vadim <70962544+FraiVadim@users.noreply.github.com>
2020-09-23 17:12:34 +0300

    Add files via upload

---
+++ operatori python . problema 5.py
@@ -0,0 +1,9 @@
+x=True 
+y=False 
+print ("x=True , y=False")
+print (" x and y","    x and x","   y and y",)
+print ("",(x and y),"     ",(x and x),"     ",(y and y))
+print (" x or y","     x or x","    y or y")
+print ("",(x or y),"      ",(x or x),"     ",(y or y))
+print (" not y ","     not x")
+print ("",(not y),"      ",(not x))
